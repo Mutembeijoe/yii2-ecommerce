0a2df673922a851d596bcce8c2d738f98c0c8928
mutembeijoe <mutembeijoe@gmail.com>
2021-01-06 08:10:08 +0300

    installs boostrap 4, integrates a bootstrap template nad configures the AppAsset

--- backend/config/main.php
+++ backend/config/main.php
@@ -37,6 +37,9 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
+        'assetManager' => [
+            'bundles' => [\yii\bootstrap4\BootstrapAsset::class => false],
+        ]
         /*
         'urlManager' => [
             'enablePrettyUrl' => true,
